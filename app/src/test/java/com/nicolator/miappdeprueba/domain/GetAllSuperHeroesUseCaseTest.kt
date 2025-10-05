f90f107e53585eac8829fe8b692c148b7ab7fc1e
Nicolas-Savuica-educacyl <nicolasa.sav@educa.jcyl.es>
2025-10-05 11:42:47 +0200

    UnitTests done

--- app/src/test/java/com/nicolator/miappdeprueba/domain/GetAllSuperHeroesUseCaseTest.kt
+++ app/src/test/java/com/nicolator/miappdeprueba/domain/GetAllSuperHeroesUseCaseTest.kt
@@ -1,6 +1,40 @@
 package com.nicolator.miappdeprueba.domain
 
+import com.nicolator.miappdeprueba.data.SuperHeroDataRepository
+import io.mockk.coEvery
+import io.mockk.mockk
+import io.mockk.verify
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
 class GetAllSuperHeroesUseCaseTest {
+
+    @Test
+    fun `when invoke request a Result of List of superheroes` () {
+
+        //given
+        val superHeroesList = listOf(
+            SuperHero( "1",  "Superman", "", ""),
+            SuperHero( "2",  "Batman", "", "")
+        )
+        val expected = Result.success(superHeroesList)
+
+        val dataRepositoryMockk = mockk<SuperHeroDataRepository>(relaxed = true)
+        coEvery { dataRepositoryMockk.getAllSuperheroes() } returns expected
+
+        val getAllSuperHeroesUseCase = GetAllSuperHeroesUseCase(dataRepositoryMockk)
+
+        //when
+        val result = getAllSuperHeroesUseCase.invoke()
+
+         //then
+
+        verify (exactly = 1) { dataRepositoryMockk.getAllSuperheroes()  }
+        assertEquals(expected, result)
+
+    }
+
+
 
 
 }
---
+++ app/src/test/java/com/nicolator/miappdeprueba/domain/GetSuperHeroByIdUseCaseTest.kt
@@ -0,0 +1,13 @@
+package com.nicolator.miappdeprueba.domain
+
+import com.nicolator.miappdeprueba.data.SuperHeroDataRepository
+import io.mockk.coEvery
+import io.mockk.mockk
+import io.mockk.verify
+import junit.framework.TestCase
+import org.junit.Assert.*
+import org.junit.Test
+
+class GetSuperHeroByIdUseCaseTest {
+    //TODO
+}
